be82c87bacbbfe9a6dc8b51e13daf5915d70130e
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-10-04 00:14:59 +0500

    домашка 2.14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
+    private static final String[] otherList = new String[6];
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringList stringList = new StringListImpl();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+        System.out.println( "1. " + Arrays.toString(stringList.toArray()));
+
+        stringList.add(4, "e");
+        System.out.println("2. " + Arrays.toString(stringList.toArray()));
+
+        stringList.set(2, "P");
+        System.out.println("3. " + Arrays.toString(stringList.toArray()));
+
+        stringList.remove("P");
+        System.out.println("3. " + Arrays.toString(stringList.toArray()));
+
+        stringList.remove(1);
+        System.out.println("4. " + Arrays.toString(stringList.toArray()));
+
+        System.out.println("5. " + Arrays.toString(new boolean[]{stringList.contains("a")}));
+        System.out.println("6. " + Arrays.toString(new boolean[]{stringList.contains("v")}));
+
+        System.out.println("7. " + Arrays.toString(new int[]{stringList.indexOf("a")}));
+        System.out.println("8. " + Arrays.toString(new int[]{stringList.indexOf("m")}));
+
+        System.out.println("9. " + Arrays.toString(new int[]{stringList.lastIndexOf("b")}));
+        System.out.println("10. " + Arrays.toString(new int[]{stringList.lastIndexOf("d")}));
+
+        System.out.println("11. " + Arrays.toString(new String[]{stringList.get(1)}));
+        
+        System.out.println("12. " + Arrays.toString(new boolean[]{stringList.equals(otherList)}));
+
+        System.out.println("13. " + Arrays.toString(new int[]{stringList.size()}));
+
+        System.out.println("14. " + Arrays.toString(new boolean[]{stringList.isEmpty()}));
+
+        stringList.clear();
+        System.out.println("15. " + Arrays.toString(stringList.toArray()));
+
+        stringList.toArray();
+        System.out.println("16. " + Arrays.toString(stringList.toArray()));
     }
+
 }
---
+++ src/main/java/org/example/StringListImpl.java
@@ -0,0 +1,196 @@
+package org.example;
+
+import java.util.Objects;
+
+public class StringListImpl implements StringList{
+
+    private static final int INITIAL_SIZE = 20;
+
+    private final String[] data;
+
+    private int capacity;
+
+    public StringListImpl() {
+        if (INITIAL_SIZE <= 0) {
+            throw new IllegalArgumentException("Размер списка должен быть положительным");
+        }
+        data = new String[INITIAL_SIZE];
+        capacity = 0;
+    }
+
+    @Override
+    public String add(String item) {
+        if (capacity >= data.length) {
+            throw new IllegalArgumentException("Список полон");
+        }
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Добавлять null нельзя");
+        }
+        data[capacity] = item;
+        capacity++;
+        return item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        if (capacity >= data.length) {
+            throw new IllegalArgumentException("Список полон");
+        }
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Добавлять null нельзя");
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным");
+        }
+        if (index > capacity) {
+            throw new IllegalArgumentException("Индекс должен имень значение " + capacity);
+        }
+        // Копирует и сдвигает
+        // Пример. а б в г д null null null ... null
+        // а б в в г д null ... null
+        System.arraycopy(data, index, data, index + 1, capacity - index);
+        // Ставим ш вместо первой в
+        // а б ш в г д null ... null
+        data[index] = item;
+        capacity++;
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Добавлять null нельзя");
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным");
+        }
+        if (index > capacity) {
+            throw new IllegalArgumentException("Индекс не может быть больше значения " + capacity);
+        }
+        // Пишем вместо старого значения новое на тоже место
+        return data[index] = item;
+    }
+
+    @Override
+    public String remove(String item) {
+        int removing = indexOf(item);
+        return remove(removing);
+        // Возвращаем remove(index) и работаем в следующем remove (public String remove (int index))
+    }
+
+    @Override
+    public String remove(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным");
+        }
+        if (index >= capacity) {
+            throw new IllegalArgumentException("Индекс не может быть больше значения " + (capacity - 1));
+        }
+        String removed = data[index];
+        // Копирует и сдвигает
+        // Пример. а б в г д null null ... null
+        // Удаляем в, копируем и сдвигаем г д: а б г д д null null ... null
+        System.arraycopy(data, index + 1, data, index, capacity - 1 - index);
+        capacity--;
+        // Ставим вместо второй д null
+        // Получилось: а б г д null null null ... null
+        data[capacity] = null;
+        return removed;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Наличие null не проверяем");
+        }
+        for (int i = 0; i < capacity; i++) {
+            if (data[i].equals(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Наличие null не проверяем");
+        }
+        int index = -1;
+        for (int i = 0; i < capacity; i++) {
+            if (data[i].equals(item)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        if (Objects.isNull(item)) {
+            throw new IllegalArgumentException("Наличие null не проверяем");
+        }
+        int index = -1;
+        for (int i = capacity - 1; i >= 0; i--) {
+            if (data[i].equals(item)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    @Override
+    public String get(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Индекс не может быть отрицательным");
+        }
+        if (index > capacity) {
+            throw new IllegalArgumentException("Индекс не может быть больше значения " + capacity);
+        }
+        return data[index];
+    }
+
+    @Override
+    public boolean equals(StringList otherList, Object list) {
+        if (Objects.isNull(otherList)) {
+            throw new IllegalArgumentException("null не проверяем");
+        }
+        if (size() != otherList.size()) {
+            return false;
+        }
+        for (int i = 0; i < capacity; i++) {
+            if (!data[i].equals(otherList.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // Если это условие выполняется, то возвращается true
+        return size() == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            data[i] = null;
+        }
+        capacity = 0;
+    }
+
+    @Override
+    public String[] toArray() {
+        String[] result = new String[capacity];
+        System.arraycopy(data, 0, result, 0, capacity);
+        return result;
+    }
+}
